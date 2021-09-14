2099498de9d9554cb3376def6ac8b4ba3be08c00
Abdelmajed-zeyad <abd.d@live.com>
2021-09-14 18:55:43 +0300

    edit css

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,7 @@ export default function Home() {
 
   }
 
-  // function getLatestReply() {
-  //   return answeredQuestions[answeredQuestions.length - 1]?.reply || 'Ask me anything!'
-  // }
+
 
   return (
     <div className="">
@@ -36,26 +34,26 @@ export default function Home() {
         </Header>
 
         <form className=" w-3/4 p-2 mx-auto my-4 bg-green-200" onSubmit={CookiesPostHandler}>
-          <p>Create  Cookies Stand  </p>
-          <div className="flex">
-            <p>Location</p>
-            <input name="Location" className="w-3/4" />
+          <p className='text-xl  text-center  text-green-800'>Create  Cookies Stand  </p>
+          <div className=" flex justify-start ">
+            <p className='my-3' >Location</p>
+            <input name="Location" className=" w-5/6  mx-5 my-3" />
           </div>
 
-          <div className="flex w-4/5	">
-            <div className="w-2/5 m-2	" >
-              <p> Minimum Customers per Hour</p>
-              <input name="MinCus" className="" />
+          <div className="flex  justify-center my-5 py-4 bg-green-300	">
+            <div className=" ml-4 flex-1	" >
+              <p className=' text-center  h-17'> Minimum Customers per Hour</p>
+              <input name="MinCus" className="w-full" />
             </div>
-            <div className="w-2/5 m-2">
-              <p> Maximum Customers per Hour</p>
-              <input name="MaxCus" className="" />
+            <div className=" ml-4  flex-1">
+              <p className=' text-center h-17'> Maximum Customers per Hour</p>
+              <input name="MaxCus" className="w-full" />
             </div>
-            <div className="w-2/5 m-2">
-              <p> Average Cookies per Sale</p>
-              <input name="AvgCokPH" className="" />
+            <div className=" ml-4 flex-1">
+              <p className=' text-center h-17'> Average Cookies per Sale</p>
+              <input name="AvgCokPH" className=" w-full" />
             </div>
-            <button className="px-8 py-1 bg-green-500 text-gray-50">Create</button>
+            <button className="  flex-1 bg-green-500 text-gray-50 mx-4 my-auto mt-3" >Create</button>
 
           </div>
         </form>
